6d99c48a38a1b62ebc5cb78f82f84a0af5281bc6
Mishu Rahman <mishu.cse616@gmail.com>
2024-02-04 13:08:31 +0600

    Custom Exception Added

---
+++ src/StackOverflow.BL/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace StackOverflow.BL.Exceptions
+{
+    public class NotFoundException: Exception
+    {
+        public NotFoundException(string message):base(message) { }
+    }
+}
---
+++ src/StackOverflow.BL/Exceptions/PermissionMissingException.cs
@@ -0,0 +1,10 @@
+﻿namespace StackOverflow.BL.Exceptions
+{
+    public class PermissionMissingException: Exception
+    {
+        public PermissionMissingException(string message) : base(message)
+        {
+
+        }
+    }
+}
