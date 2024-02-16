cd5565b73e57377dcd5fbcee53ebe4859d48fb32
Mishu Rahman <mishu.cse616@gmail.com>
2024-02-17 01:49:05 +0600

    Question Category table added

---
+++ src/StackOverflow.DAL/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using StackOverflow.DAL.Entities.Base;
+
+namespace StackOverflow.DAL.Entities
+{
+    public class Category
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/StackOverflow.DAL/Mappings/CategoryMap.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using StackOverflow.DAL.Entities;
+
+namespace StackOverflow.DAL.Mappings
+{
+    public class CategoryMap: ClassMapping<Category>
+    {
+        public CategoryMap()
+        {
+            Table("Categories");
+
+            Id(cat => cat.Id, map =>
+            {
+                map.Generator(Generators.GuidComb);
+            });
+
+            Property(cat => cat.Name, map =>
+            {
+                map.Length(20);
+                map.Unique(true);
+            });
+
+            Property(cat => cat.Description, map =>
+            {
+                map.Length(1000);
+            });
+
+        }
+    }
+}
